6b1c8cdf56e2fc3c5dc2fb16c848b05b25aac96d
Emerson04-sv <balmore04ebao@gmail.com>
2021-03-07 19:24:06 -0600

    INDEX: agregue la pantalla VF, CSS: coloque las posiciones, JS:agarre los objetos y estoy validando el array

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("hola mundo");
+const btnValidarTajeta = document.getElementById("btn-ValidarTarjeta");
+const mostrarError= document.getElementById("AnuncioDeError");
+let numTarjetaAux="";
+
+
+btnValidarTajeta.addEventListener("click", function(){
+    rellenarInputCorrectamente();
+});
+
+function rellenarInputCorrectamente(numTajetaAux){
+    const numTarjetaIngresada= document.getElementById("input-ColocarTarjeta").value;
+    numTarjetaAux=numTarjetaIngresada.trim();
+    if(numTarjetaAux ==="")
+    {
+        mostrarError.innerText="No ha ingresado nada";
+    }
+    else if(numTarjetaAux.length!==16)
+    {
+        mostrarError.innerText="Ha ingresado "+numTarjetaAux.length+" digitos pero son 16 digitos";
+    }
+    else
+    {
+        revisarSiSonNumeros(numTarjetaAux);
+    }
+}
+function revisarSiSonNumeros(numTarjetaAux){
+    let numTarjetaArray=[];
+    numTarjetaArray=Array.from(numTarjetaAux);
+    for(let i=0; i<=15; i++)
+    {
+        numTarjetaArray[i]=parseInt(numTarjetaArray[i] , 10);
+        if(numTarjetArray[i]!==i)
+        {
+            mostrarError.innerText="Lo siento, ha ingresado un dato no numérico";
+        }
+        else
+        {
+            mostrarPatallaVerdaderaOFalsa();
+        }
+    }
+}
+function mostrarPatallaVerdaderaOFalsa(){3
+
+    mostrarError.innerText="vamo bien";
+}
